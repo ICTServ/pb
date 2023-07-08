3475225ebadab10797021e7a7bdcaa9c8bcc5461
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 05:51:05 +0300

    Update pbinstall.sh

--- pbinstall.sh
+++ pbinstall.sh
@@ -14,9 +14,11 @@ while getopts d:e: opt; do
 done
 certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos  && 
 /root/pb/pocketbase serve --http=$domain:80 --https=$domain:443  &&
-certbot renew &&
-crontab -e &&
-echo "45       2       *       *       6       certbot renew | crontab -e" 
+
+
+# certbot renew &&
+# crontab -e &&
+# echo "45       2       *       *       6       certbot renew | crontab -e" 
 
 # certbot renew &&
 # crontab -e &&
