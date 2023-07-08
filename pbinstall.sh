65e4d9314170d03cc52e41bde4d9dea19c07df46
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 05:42:22 +0300

    Update pbinstall.sh

--- pbinstall.sh
+++ pbinstall.sh
@@ -13,12 +13,25 @@ while getopts d:e: opt; do
     esac
 done
 certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos  && 
-/root/pb/pocketbase serve --http=$domain:80 --https=$domain:443  &&
+# /root/pb/pocketbase serve --http=$domain:80 --https=$domain:443  &&
+certbot renew &&
+crontab -e &&
+echo "45       2       *       *       6       certbot renew" >> crontab -e &&
+echo -e "
+name=$RC_SVCNAME  \n
+description="pocketbase"  \n
+supervisor="supervise-daemon"  \n
+command="/var/app/pb/pocketbase"  \n
+command_args="serve --http="$domain:80" --https="$domain:443""  \n
+command_user="root:root" \n
+" >> /etc/init.d/pocketbase  &&
+rc-update add pocketbase default && 
+service pocketbase start 
 
 # certbot certonly --standalone -n -d example.com --staple-ocsp -m email@example.com --agree-tos  && 
 # /var/app/pb/pocketbase serve --http=example.com:80 --https=example.com:443  &&
-certbot renew &&
-crontab -e &&
-echo `45       2       *       *       6       certbot renew | crontab -e` 
+# certbot renew &&
+# crontab -e &&
+# echo `45       2       *       *       6       certbot renew | crontab -e` 
 
 # wget http://raw.githubusercontent.com/ICTServ/pb/main/pbinstall.sh -O pbinstall.sh  && sh pbinstall.sh -d “example.com” -e “email@example.com”
