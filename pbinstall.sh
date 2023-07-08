af512dc385a5354ac2e79a262d576b9117ec6e28
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 05:00:55 +0300

    Update pbinstall.sh

--- pbinstall.sh
+++ pbinstall.sh
@@ -15,6 +15,6 @@ unzip -o -qq /tmp/pb.zip -d /var/app  &&
 certbot certonly --standalone -n -d cotech.site --staple-ocsp -m email@cotech.site --agree-tos  && 
 /root/pb/pocketbase serve --http=cotech.site:80 --https=cotech.site:443  &&
 
-certbot renew \
-crontab -e \
+certbot renew &&
+crontab -e &&
 echo `45       2       *       *       6       certbot renew | crontab -e` 
