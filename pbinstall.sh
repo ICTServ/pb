500691d4fe386f0773c15c2a9f290f8181ee3f6c
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 04:56:41 +0300

    Update pbinstall.sh

--- pbinstall.sh
+++ pbinstall.sh
@@ -3,14 +3,18 @@
 mkdir -p /var/app &&
 wget -O /tmp/pb.zip "$(curl -s https://api.github.com/repos/pocketbase/pocketbase/releases/assets/115395053 |  grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')"  &&
 unzip -o -qq /tmp/pb.zip -d /var/app  &&
-while getopts d:e: opt; do
-    case "$opt" in
-        d) domain=$OPTARG;;
-        e) email=$OPTARG;;
-    esac
-done
-certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos  && 
-/root/pb/pocketbase serve --http=$domain:80 --https=$domain:443  &&
+# while getopts d:e: opt; do
+#     case "$opt" in
+#         d) domain=$OPTARG;;
+#         e) email=$OPTARG;;
+#     esac
+# done
+# certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos  && 
+# /root/pb/pocketbase serve --http=$domain:80 --https=$domain:443  &&
+
+certbot certonly --standalone -n -d cotech.site --staple-ocsp -m email@cotech.site --agree-tos  && 
+/root/pb/pocketbase serve --http=cotech.site:80 --https=cotech.site:443  &&
+
 certbot renew \
 crontab -e \
 echo `45       2       *       *       6       certbot renew | crontab -e` 
