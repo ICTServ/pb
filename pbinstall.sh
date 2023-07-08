05d9ad2e706f95762b342c87d77a44a6bc59ad5f
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 04:45:10 +0300

    Update pbinstall.sh

--- pbinstall.sh
+++ pbinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apk update && apk add certbot && apk add curl && apk add zip  \ 
+# apk update && apk add certbot && apk add curl && apk add zip  \ 
 mkdir -p /var/app
 wget -O /tmp/pb.zip "$(curl -s https://api.github.com/repos/pocketbase/pocketbase/releases/assets/115395053 |  grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')"   \
 unzip -o -qq /tmp/pb.zip -d /var/app   \ 
