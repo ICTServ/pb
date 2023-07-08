bf6b1fbd6e139e2cd1606b04b50a8a912d3fcac0
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 03:21:26 +0300

    Update pb-install.sh

--- pb-install.sh
+++ pb-install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 apk update  && apk add certbot curl wget zip
-wget -b -O pb.zip "$(curl -s "https://api.github.com/repos/pocketbase/pocketbase/releases/assets/115395053" |  grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')" &&
-unzip -o -qq  pb.zip -d pb  &&
-mkdir -p /var/app  &&
-cp -R /root/pb /var/app  &&
+wget -b -O pb.zip "$(curl -s "https://api.github.com/repos/pocketbase/pocketbase/releases/assets/115395053" |  grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')"
+unzip -o -qq  pb.zip -d pb
+mkdir -p /var/app
+cp -R /root/pb /var/app
 
-while getopts "d:e:" opt; do
-  case $opt in
-    d) domain=$OPTARG ;;
-    e) email=$OPTARG ;;
-  esac
+while getopts d:e: flag
+do
+    case "${flag}" in
+        d) domain=${OPTARG};;
+        e) email=${OPTARG};;
+    esac
 done
 
-certbot certonly --standalone -d $domain --staple-ocsp -m $email --agree-tos -n &&
-cd /root/pb/ &&
-./pocketbase serve --https=$domain:443
+certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos 
+/root/pb/pocketbase serve --http=$domain:80 --https=$email:443
