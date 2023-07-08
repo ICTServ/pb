d54e1b99287cc44355b716552a5d3b402b0caa63
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 03:00:07 +0300

    Update pb-install.sh

--- pb-install.sh
+++ pb-install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 apk update  && apk add certbot curl wget zip
 wget -b -O pb.zip "$(curl -s "https://api.github.com/repos/pocketbase/pocketbase/releases/assets/115395053" |  grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')" &&
-unzip -o -qq  pb.zip -d pb
-mkdir -p /var/app
-cp -R /root/pb /var/app
+unzip -o -qq  pb.zip -d pb  &&
+mkdir -p /var/app  &&
+cp -R /root/pb /var/app  &&
 
 while getopts d:e: flag
 do
@@ -13,5 +13,6 @@ do
     esac
 done
 
-certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos 
-/root/pb/pocketbase serve --http=$domain:80 --https=$email:443
+certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos &&
+cd /root/pb/ &&
+./pocketbase serve --http=$domain:80 --https=$domain:443
