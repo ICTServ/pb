16d9ca2ab9a11373bd6f0feffd07f542ddab22d5
ICTServ <91088703+ICTServ@users.noreply.github.com>
2023-07-08 03:39:14 +0300

    Update pbinstall.sh

--- pbinstall.sh
+++ pbinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apk add certbot curl zip  \ 
+apk add certbot && apk add curl && apk add zip  \ 
 wget -b -O pb.zip "$(curl -s "https://api.github.com/repos/pocketbase/pocketbase/releases/assets/115395053" |  grep '"browser_download_url":' | sed -E 's/.*"([^"]+)".*/\1/')"   \
 unzip -o -qq  pb.zip -d pb   \ 
 mkdir -p /var/app  && 
@@ -15,7 +15,6 @@ done
 
 certbot certonly --standalone -n -d $domain --staple-ocsp -m $email --agree-tos   \ 
 /root/pb/pocketbase serve --http=$domain:80 --https=$domain:443  \
-certbot renew --dry-run // check \
 certbot renew \
 crontab -e \
 echo "45       2       *       *       6       certbot renew" | crontab -e" \
